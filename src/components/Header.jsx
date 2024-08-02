a960cf6471b174e478043a7e44bb5de2ebe8e804
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-08-02 13:22:07 -0400

    Install react-router-dom and update Header,jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Developer Name</h1>
+      {/* Navigation links will go here */}
+    </header>
+  );
+}
+
+export default Header;

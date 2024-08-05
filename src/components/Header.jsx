91dd130e8d974140b35d314884f218f2a40807b4
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-08-05 15:05:53 -0400

    Install vitest, and test libraries from reat and jest-dom. Update header component and test file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <h1>Developer Name</h1>
-      {/* Navigation links will go here */}
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>About Me</Link>
+          </li>
+          <li>
+            <Link to='/portfolio'>Portfolio</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li>
+            <Link to='/resume'>Resume</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }

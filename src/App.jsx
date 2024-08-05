e0e95f6578c59e1bd86288da42d7d2d89f7e1e1e
Daniel Rios <ever.rios.gonzalez@gmail.com>
2024-08-05 19:21:53 -0400

    Update component and page route export

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      // <Outlet /> {/* Add this Outlet component */}
-      // <Footer />
+      <Outlet /> {/* Add this Outlet component */}
+      <Footer />
     </>
   );
 }
---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,13 @@
+// src/pages/AboutMe.jsx
+import React from "react";
+
+function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is the About Me page content.</p>
+    </div>
+  );
+}
+
+export default AboutMe;
